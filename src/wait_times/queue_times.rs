3147d7c7d5834705c9024ad00de043f5152c720a
adamZdrojewski <adam.zdrojewski2@gmail.com>
2026-03-01 12:51:48 -0500

    feat: add feature to load mock queue times response data from file

--- src/wait_times/queue_times.rs
+++ src/wait_times/queue_times.rs
@@ -13,20 +13,28 @@ struct QueueTimesLand {
 }
 
 #[derive(Deserialize, Debug)]
-struct QueueTimesRide {
-    id: u32,
-    name: String,
-    is_open: bool,
-    wait_time: u32,
-    last_updated: String
+pub struct QueueTimesRide {
+    pub id: u32,
+    pub name: String,
+    pub is_open: bool,
+    pub wait_time: u32,
+    pub last_updated: String
 }
 
 async fn fetch_wait_time_data() -> Result<QueueTimesResponse, reqwest::Error> {
-    let url = "https://queue-times.com/parks/50/queue_times.json";
+    #[cfg(not(feature = "mock"))]
+    {
+        let url = "https://queue-times.com/parks/50/queue_times.json";
+        let response = reqwest::get(url).await?;
+        let wait_time_data: QueueTimesResponse = response.json().await?;
+        Ok(wait_time_data)
+    }
 
-    let response = reqwest::get(url).await?;
-
-    let wait_time_data: QueueTimesResponse = response.json().await?;
-
-    Ok(wait_time_data)
+    #[cfg(feature = "mock")]
+    {
+        println!("Loading mock data from file...");
+        let file_data = std::fs::read_to_string("mock-data.json").unwrap();
+        let data = serde_json::from_str(&file_data).unwrap();
+        Ok(data)
+    }
 }
